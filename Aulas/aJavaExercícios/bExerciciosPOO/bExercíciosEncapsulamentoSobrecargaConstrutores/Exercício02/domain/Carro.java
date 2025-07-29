52b71e239ad01aa268d2e935baee5efbb4ef7983
felipemrj <157051881+felipemrj@users.noreply.github.com>
2025-07-29 17:08:53 -0300

    Exercícios

--- Aulas/aJavaExercícios/bExerciciosPOO/bExercíciosEncapsulamentoSobrecargaConstrutores/Exercício02/domain/Carro.java
+++ Aulas/aJavaExercícios/bExerciciosPOO/bExercíciosEncapsulamentoSobrecargaConstrutores/Exercício02/domain/Carro.java
@@ -1,4 +1,50 @@
 package aJavaExercícios.bExerciciosPOO.bExercíciosEncapsulamentoSobrecargaConstrutores.Exercício02.domain;
 
 public class Carro {
+    private String marca;
+    private String modelo;
+    private int ano;
+
+
+    public Carro(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public Carro(String marca, String modelo, int ano) {
+        this(marca, modelo);
+        this.ano = ano;
+    }
+
+    public void acelerar() {
+        System.out.println("O carro está acelerando");
+    }
+
+    public void acelerar(int velocidade) {
+        System.out.printf("O carro está acelerando a %d km/h", velocidade);
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public int getAno() {
+        return this.ano;
+    }
 }
--- Aulas/aJavaExercícios/bExerciciosPOO/bExercíciosEncapsulamentoSobrecargaConstrutores/Exercício02/test/CarroTest01.java
+++ Aulas/aJavaExercícios/bExerciciosPOO/bExercíciosEncapsulamentoSobrecargaConstrutores/Exercício02/test/CarroTest01.java
@@ -1,4 +1,13 @@
 package aJavaExercícios.bExerciciosPOO.bExercíciosEncapsulamentoSobrecargaConstrutores.Exercício02.test;
 
+import aJavaExercícios.bExerciciosPOO.bExercíciosEncapsulamentoSobrecargaConstrutores.Exercício02.domain.Carro;
+
 public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carroUm = new Carro("Audi", "TT");
+        Carro carroDois = new Carro("Ferrari", "Super", 2025);
+
+        carroUm.acelerar();
+        carroDois.acelerar(200);
+    }
 }
