e1d6c7cb6dc79f004e205e761c86bba6bad24623
felipemrj <157051881+felipemrj@users.noreply.github.com>
2025-07-29 17:57:14 -0300

    Exercícios

--- Aulas/aJavaExercícios/bExerciciosPOO/bExercíciosEncapsulamentoSobrecargaConstrutores/Exercício05/domain/Aluno.java
+++ Aulas/aJavaExercícios/bExerciciosPOO/bExercíciosEncapsulamentoSobrecargaConstrutores/Exercício05/domain/Aluno.java
@@ -1,4 +1,29 @@
 package aJavaExercícios.bExerciciosPOO.bExercíciosEncapsulamentoSobrecargaConstrutores.Exercício05.domain;
-
+/*
+Crie uma classe chamada Aluno com os atributos privados nome (String), matricula (String) e nota (double).
+Implemente os métodos getters e setters e construtores. Implemente um método chamado verificarAprovacao()
+que retorna se o aluno está aprovado (nota >= 7). Sobrecargue esse método para permitir definir o valor mínimo de aprovação.
+ */
 public class Aluno {
+    private String nome;
+    private String matricula;
+    private int nota;
+
+    private void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    private String getNome() {
+        return this.nome;
+    }
+
+    private void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    private String getMatricula() {
+        return this.matricula;
+    }
+
+
 }
