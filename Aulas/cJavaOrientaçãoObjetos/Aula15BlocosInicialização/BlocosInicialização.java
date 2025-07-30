19ae40d025eddbc957c7027f30bac2a7dd9fe45c
felipemrj <157051881+felipemrj@users.noreply.github.com>
2025-07-30 11:58:09 -0300

    Aula Blocos Inicialização

--- Aulas/cJavaOrientaçãoObjetos/Aula15BlocosInicialização/BlocosInicialização.java
+++ Aulas/cJavaOrientaçãoObjetos/Aula15BlocosInicialização/BlocosInicialização.java
@@ -4,6 +4,13 @@ public class BlocosInicialização {
     /* Bloco de inicialização é um bloco de código dentro da classe que sempre será executado ao instanciar um objeto, ele sempre será
     executado antes de qualquer método da classe independente da ordem do código
 
+    Blocos de inicialização em Java são úteis para garantir que certas operações sejam realizadas sempre que um objeto é criado,
+    independentemente de qual construtor seja chamado. Eles são executados antes do construtor e podem ser usados para inicializar
+    variáveis de instância, abrir conexões, carregar configurações ou executar qualquer lógica que precise ocorrer na criação do objeto.
+    Caso haja herança, os blocos de inicialização da superclasse são executados antes dos blocos da subclasse.
+    Além disso, é possível ter múltiplos blocos de inicialização em uma mesma classe, e eles serão executados na ordem em que aparecem no código.
+    Isso ajuda a evitar duplicação de código entre construtores e facilita a manutenção.
+
     Dica: Ao chamarmos um get que retorna uma lista, podemos usar o atalho ALT ENTER
     para criar o for mais facilmente */
 }
--- Aulas/cJavaOrientaçãoObjetos/Aula15BlocosInicialização/domain/Anime.java
+++ Aulas/cJavaOrientaçãoObjetos/Aula15BlocosInicialização/domain/Anime.java
@@ -3,6 +3,10 @@ package cJavaOrientaçãoObjetos.Aula15BlocosInicialização.domain;
 public class Anime {
     private String nome;
     private int[] episodios;
+    /* 1 - Alocado espaço em memória pro objeto
+     * 2 - Cada atributo de classe é criado e inicializado com valores default ou o que for passado
+     * 3 - Bloco de inicialização é executado
+     * 4 - Construtor é executado */
 
     {
         episodios = new int[100];
