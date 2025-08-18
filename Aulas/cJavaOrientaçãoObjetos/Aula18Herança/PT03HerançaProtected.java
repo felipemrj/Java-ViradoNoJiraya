157fa89c2f51526d0ffe581a8db984708ddd3e97
felipemrj <157051881+felipemrj@users.noreply.github.com>
2025-08-18 17:03:56 -0300

    Herança PT03

---
+++ Aulas/cJavaOrientaçãoObjetos/Aula18Herança/PT03HerançaProtected.java
@@ -0,0 +1,8 @@
+package cJavaOrientaçãoObjetos.Aula18Herança;
+
+public class PT03HerançaProtected {
+    /*
+    O modificador protected em Java permite que atributos e métodos sejam acessados pelas subclasses (mesmo que estejam em pacotes diferentes)
+    e pelas classes do mesmo pacote. Ele é útil para encapsular dados, permitindo acesso controlado apenas para quem realmente precisa, como subclasses.
+     */
+}
--- Aulas/cJavaOrientaçãoObjetos/Aula18Herança/domain/Funcionario.java
+++ Aulas/cJavaOrientaçãoObjetos/Aula18Herança/domain/Funcionario.java
@@ -8,6 +8,10 @@ public class Funcionario extends Pessoa {
         System.out.println(this.salario);
     }
 
+    public void relatorioPagamento() {
+        System.out.println("Eu "+ this.nome + " recebi o salário");
+    }
+
     public double getSalario() {
         return salario;
     }
