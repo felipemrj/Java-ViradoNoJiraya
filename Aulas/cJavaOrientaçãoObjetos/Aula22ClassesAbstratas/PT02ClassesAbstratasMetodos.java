5cb8f583add17e03780a0e8bcb94f721793cc23c
felipemrj <157051881+felipemrj@users.noreply.github.com>
2025-08-20 15:18:40 -0300

    PT02 Classes Abstratas

---
+++ Aulas/cJavaOrientaçãoObjetos/Aula22ClassesAbstratas/PT02ClassesAbstratasMetodos.java
@@ -0,0 +1,7 @@
+package cJavaOrientaçãoObjetos.Aula22ClassesAbstratas;
+
+public class PT02ClassesAbstratasMetodos {
+    /*
+    Em métodos abstract, os métodos não podem ter corpo. E a partir do momento que um método é abstrato é obrigatorio fazer a implementação nas subclasses.
+     */
+}
