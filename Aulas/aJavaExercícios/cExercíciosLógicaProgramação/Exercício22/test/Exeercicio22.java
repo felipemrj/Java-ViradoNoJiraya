46ee91e564110160480ac1a6cb6caed57dd000b1
Felipe <157051881+felipemrj@users.noreply.github.com>
2025-08-27 13:36:05 -0300

    feat: adicionar exercício 22 de lógica de programação

---
+++ Aulas/aJavaExercícios/cExercíciosLógicaProgramação/Exercício22/test/Exeercicio22.java
@@ -0,0 +1,22 @@
+package aJavaExercícios.cExercíciosLógicaProgramação.Exercício22.test;
+
+import java.util.Scanner;
+
+public class Exeercicio22 {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
+        int a, b, quociente, resto;
+
+        System.out.println("Digite o dividendo: ");
+        a = read.nextInt();
+
+        System.out.println("Digite o divisor: ");
+        b = read.nextInt();
+
+        quociente = a / b;
+        resto = a % b;
+
+        System.out.printf("Quociente: %d | Resto: %d", quociente, resto);
+    }
+}
