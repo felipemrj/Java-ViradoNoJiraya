484c93c9894ff6c8a95641408f71aa1025c170ad
Felipe <157051881+felipemrj@users.noreply.github.com>
2025-08-27 13:30:35 -0300

    feat: adicionar exercício 21 de lógica de programação

---
+++ Aulas/aJavaExercícios/cExercíciosLógicaProgramação/Exercício21/test/Exercicio21.java
@@ -0,0 +1,13 @@
+package aJavaExercícios.cExercíciosLógicaProgramação.Exercício21.test;
+
+import java.util.Random;
+
+public class Exercicio21 {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int randomInt = random.nextInt(101);
+
+        System.out.println(randomInt);
+    }
+}
