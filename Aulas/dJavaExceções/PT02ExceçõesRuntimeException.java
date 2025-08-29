0e7c86213e635202b70bee2067c251528e69511a
Felipe <157051881+felipemrj@users.noreply.github.com>
2025-08-29 14:00:33 -0300

    feat: adicionar aula exceções pt02

---
+++ Aulas/dJavaExceções/PT02ExceçõesRuntimeException.java
@@ -0,0 +1,19 @@
+package dJavaExceções;
+
+public class PT02ExceçõesRuntimeException {
+    /*
+    Existem dois tipos de exceções:
+    - Checked;
+    - Unchecked
+
+    Checked: São exceções que são filhas da classe exception diretamente e se não
+    tratadas, elas vão lançar um erro em tempo de compilação. Precisamos obrigatoriamente
+    fazer um tratamento.
+
+    Unchecked: São exceções que são filhas ou da classe RuntimeException. Quando são
+    lançadas pelo programas geralmente significa que o problema foi causado por quem
+    escreveu o código. Não é obrigatório o tratamento.
+
+
+     */
+}
---
+++ Aulas/dJavaExceções/runtime/test/RuntimeExceptionTest01.java
@@ -0,0 +1,15 @@
+package dJavaExceções.runtime.test;
+
+public class RuntimeExceptionTest01 {
+    public static void main(String[] args) {
+        // runtime exception
+        Object object = null;
+        System.out.println(object.toString());
+
+        // array out of bounds
+        int[] nums = {1,2};
+        System.out.println(nums[2]);
+
+
+    }
+}
