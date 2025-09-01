f95a09f5eb983d335b61243171e4803bcc47c18e
felipemrj <157051881+felipemrj@users.noreply.github.com>
2025-09-01 12:48:26 -0300

    feat: adicionar aula exceções pt06

---
+++ Aulas/dJavaExceções/PT06ExceçõesBlocoFinally.java
@@ -0,0 +1,9 @@
+package dJavaExceções;
+
+public class PT06ExceçõesBlocoFinally {
+    /*
+    Há situações em que após executar o try/catch queremos que algo sempre seja executado,
+    para isso utilizamos o bloco finally. Útil por exemplo em casos para fechar recursos.
+    O bloco finally é executado independente de ter uma exceção ou não.
+     */
+}
