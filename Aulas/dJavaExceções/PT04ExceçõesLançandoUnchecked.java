bef6775732f29afcd4da74a9e069f9ee93bcd9dd
felipemrj <157051881+felipemrj@users.noreply.github.com>
2025-09-01 11:26:58 -0300

    feat: adicionar aula exceções pt04

---
+++ Aulas/dJavaExceções/PT04ExceçõesLançandoUnchecked.java
@@ -0,0 +1,13 @@
+package dJavaExceções;
+
+public class PT04ExceçõesLançandoUnchecked {
+    /*
+    Para lançar novas exceções usamos "throw new"
+    Para avisar quem estiver utilizando o método colocamos "throws" na assinatura do método
+    depois dos parênteses
+    Tratamento de exceções unchecked é opcional
+    Boa prática: usar /** para documentar o método indicando as regras do método, com essa documentação
+    usando CTRL+Q podemos ver o comentário do método
+     */
+
+}
